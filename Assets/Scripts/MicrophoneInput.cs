18f944f57f4dae72a152cded258418ce86e4cedf
Ty Rickers <ty.rickers@gmail.com>
2024-04-30 20:21:55 -0600

    added script to microphone input

--- Assets/Scripts/MicrophoneInput.cs
+++ Assets/Scripts/MicrophoneInput.cs
@@ -1,18 +1,26 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class MicrophoneInput : MonoBehaviour
 {
-    // Start is called before the first frame update
+    AudioSource audioSource;
+
     void Start()
     {
-        
-    }
+        audioSource = GetComponent<AudioSource>();
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // Check if there is at least one microphone connected
+        if (Microphone.devices.Length <= 0)
+        {
+            Debug.LogWarning("Microphone not found");
+            return;
+        }
+
+        // Get the default microphone recording capabilities
+        audioSource.clip = Microphone.Start(Microphone.devices[0], true, 10, 44100);
+        audioSource.loop = true; // Set the AudioClip to loop
+        // Check that the microphone is recording, if not, start the playback
+        while (!(Microphone.GetPosition(null) > 0)) { }
+        audioSource.Play(); // Play the audio source!
     }
 }
